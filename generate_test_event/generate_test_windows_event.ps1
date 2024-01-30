9fee8caa4a1668aaf451e0c98fd61984c022c4de
josephkonieczka <15926710+josephkonieczka@users.noreply.github.com>
2024-01-30 16:52:34 -0600

    Update generate_test_windows_event.ps1
    Added the following:
    Parameters and for loop to generate multiple events. 
    Sleep to allow events to be created before Get-WinEvent
    FilterHashTable shows how Write and Get parameters are named differently for the exact same fields.

--- generate_test_event/generate_test_windows_event.ps1
+++ generate_test_event/generate_test_windows_event.ps1
@@ -1,19 +1,40 @@
-$demoTime = (Get-Date).AddMinutes(-1.5)
-$message = "The previous system shutdown at $($demoTime) was unexpected."
-Write-EventLog -LogName "System" -Source "Event Log" -EventID 6008 -EntryType ERROR -Message $message
-Write-Output "Ten Minutes ago timeframe for 6008"
-(Get-Date).AddMinutes(-10)
+param (
+    $minutesBack = -1.5,
+    $demoTime = (Get-Date).AddMinutes($minutesBack),
+    $message = "The previous system shutdown at $($demoTime) was unexpected.",
+    $LogName = 'System',
+    $Source = 'Event Log',
+    $ID = 6008,
+    $EntryType = 'ERROR',
+    $minutesStartTime = -2,
+    $minutesEndTime = 0,
+    $numEvents = 5
+)
 
-<#
+for ($i = 0; $i -lt $numEvents; $i++)
+{
+    Write-EventLog -LogName $LogName -Source $Source -EventId $ID -EntryType $EntryType -Message $message
+}
 
-"System"; - 
-"Event Log";
-6008;
-2; 
--10; How many minutes back?
-0; When does it end? 0 means NOW
-Custom Metrics|WindowsEventLogMonitor|UnexpectedShutdown_6008|EventCount
+Write-Output 'Sleeping for 30 seconds to allow events to be created.'
+Start-Sleep 30
 
-#>
+$now = Get-Date
+$StartTime = $now.AddMinutes($minutesStartTime)
+$EndTime = $now.AddMinutes($minutesEndTime)
+        
+$filterHash = @{
+    LogName      = $LogName
+    ProviderName = $Source
+    ID           = $ID
+    StartTime    = $StartTime
+    EndTime      = $EndTime
+}
 
-#
+#$filterHash
+
+
+$filteredEvents = Get-WinEvent -FilterHashtable $filterHash -ErrorAction Stop #-Verbose
+$eventCount = $filteredEvents.Count
+
+Write-Output "$($eventCount) events were found."
